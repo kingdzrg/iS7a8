d08d2f89486b802952fa6996a9d367bf7788faf9
kingdzrg <58010386+kingdzrg@users.noreply.github.com>
2020-02-08 19:38:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,17 @@ client.on("message", message => {
   });
 
 
-
+client.on('message', message => {
+    if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('kibc')){
+if(!message.author.id === '537675177677291530') return;
+message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
 
 
 
